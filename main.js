c56a0afcf6061443a8e6429ff180ca6662d423fb
Lucas Adriano <lukasadriano94@hotmail.com>
2024-07-19 17:53:13 -0300

    quase pronto

--- main.js
+++ main.js
@@ -1,5 +1,14 @@
-function tocaSom(idElementoAudio) {
-    document.querySelector(idElementoAudio).play();
+function tocaSom(seletorAudio) {
+    const elemento = document.querySelector(seletorAudio);
+
+    if (elemento === null) {
+        alert('Elemento não encontrado');
+    }
+    if (elemento != null) {
+        if (elemento.localName === 'audio') {
+            elemento.play();
+        }
+    }
 }
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
@@ -11,8 +20,19 @@ for (let contador =0; contador < listaDeTeclas.length; contador++) {
     const instrumento = listaDeTeclas[contador].classList[1];
     const idAudio = `#som_${instrumento}`;//template String 
 
-    tecla.onclick = function () {
+    tecla.onclick = function () {//clicar no botão reproduz audio
         tocaSom(idAudio);
-    };
-    
+    }
+
+    tecla.onkeydown = function(evento){//mostra botão quando precionado
+        
+        if (evento.code ==='Space' || evento.code ==='Enter') {
+            tecla.classList.add('ativa');
+        }
+    }
+
+    tecla.onkeyup = function(){//volta o botão apos soltar a tecla
+        tecla.classList.remove('ativa');
+    }
 }
+
