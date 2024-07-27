7a46b1f6e5785b6f4c691259951c3aef8fe19899
Lucas Adriano <lukasadriano94@hotmail.com>
2024-07-27 09:52:45 -0300

    Projeto finalizado

--- main.js
+++ main.js
@@ -1,20 +1,19 @@
 function tocaSom(seletorAudio) {
     const elemento = document.querySelector(seletorAudio);
 
-    if (elemento === null) {
-        alert('Elemento não encontrado');
+    if (elemento && elemento.localName === 'audio') {
+        elemento.play();
     }
-    if (elemento != null) {
-        if (elemento.localName === 'audio') {
-            elemento.play();
-        }
+    else {
+        alert('Elemento ou Seletor inválido');
+
     }
 }
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
 
 //Para
-for (let contador =0; contador < listaDeTeclas.length; contador++) {
+for (let contador = 0; contador < listaDeTeclas.length; contador++) {
 
     const tecla = listaDeTeclas[contador];
     const instrumento = listaDeTeclas[contador].classList[1];
@@ -24,14 +23,14 @@ for (let contador =0; contador < listaDeTeclas.length; contador++) {
         tocaSom(idAudio);
     }
 
-    tecla.onkeydown = function(evento){//mostra botão quando precionado
-        
-        if (evento.code ==='Space' || evento.code ==='Enter') {
+    tecla.onkeydown = function (evento) {//mostra botão quando precionado
+
+        if (evento.code === 'Space' || evento.code === 'Enter') {
             tecla.classList.add('ativa');
         }
     }
 
-    tecla.onkeyup = function(){//volta o botão apos soltar a tecla
+    tecla.onkeyup = function () {//volta o botão apos soltar a tecla
         tecla.classList.remove('ativa');
     }
 }
